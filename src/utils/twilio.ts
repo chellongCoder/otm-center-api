0b4336aa6ca43b82642ee10544bde57306000d32
Cao Hoàng Huy <hoanghuychh@gmail.com>
2023-08-12 10:35:12 +0700

    replace authkey twillo

--- src/utils/twilio.ts
+++ src/utils/twilio.ts
@@ -1,7 +1,7 @@
 import config from 'config';
 
 const accountSid = config.get('twilio.accountSID');
-const authToken = config.get('twilio.authToken');
+const authToken = process.env.AUTH_TWILLO;
 const TWILIO_SERVICE_ID = config.get('twilio.verifySid');
 
 export const twilioService = require('twilio')(accountSid, authToken).verify.v2.services(TWILIO_SERVICE_ID);
