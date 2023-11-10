96c501602668a3dda58f175270b790c024d5d645
Longnn <longnn17.developer@gmail.com>
2023-11-10 16:12:39 +0700

    fix(): marking student

--- src/services/class-timetable-details.service.ts
+++ src/services/class-timetable-details.service.ts
@@ -399,7 +399,7 @@ export class ClassTimetableDetailsService {
   public async updateClassTimetableDetailMarking(id: number, item: UpdateClassTimetableDetailMarkingDto, userWorkspaceId: number) {
     const classTimetableDetailData = await ClassTimetableDetails.findOne({
       where: {
-        timetableId: id,
+        id,
       },
       relations: ['userWorkspace', 'userWorkspace.userWorkspaceDevices', 'timetable'],
     });
